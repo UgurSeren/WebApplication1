206b99611adbf42956c67b8cf9ad7670f667e291
selim burak <ugrseren@hotmail.com>
2023-05-24 15:08:27 +0200

    Projektdateien hinzufügen.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+
+        public ViewResult Index()
+        {
+            
+
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks",guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(s => s.WillAttend==true));
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication4/Models/Class.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string? name { set; get; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress]
+        public string? Email { set; get; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string? Phone { set; get; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { set; get; }
+
+    }
+}
